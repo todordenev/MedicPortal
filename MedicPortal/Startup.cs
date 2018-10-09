ab999dcda0fd2b58d33d9f8615e27493f7f503c2
Todor Denev <todor.denev@gmail.de>
2018-10-09 14:41:53 +0200

    Test

--- MedicPortal/Startup.cs
+++ MedicPortal/Startup.cs
@@ -81,8 +81,7 @@ namespace MedicPortal
                 ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
 
                 ValidateAudience = false,
-                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
-
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)], 
                 ValidateIssuerSigningKey = true,
                 IssuerSigningKey = _signingKey,
 
